33dd3d23e614f36a8681bcc8bc3bb4406f1b682c
= <60821161+Agua-bit@users.noreply.github.com>
2023-10-08 22:28:36 -0500

    Creadas Clases Carrito, Productos, Rastreo

---
+++ src/compras/Administrador.java
@@ -0,0 +1,14 @@
+package compras;
+import Caja;
+import java.util.ArrayList;
+import java.util.List;
+public class Administrador {
+
+    private Carrito Carrito;
+    private Bodega Bodega;
+    private Productos Productos;
+    private Fabrica Fabrica;
+    private Rastreo Rastreo;
+    private Caja Caja;
+
+
