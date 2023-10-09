06f1a178281df6f97850485a4b33e0374d70404b
Zuleto1231 <mzueta@unal.edu.co>
2023-10-08 22:45:39 -0500

    Commit carrito base + setter y getters de fabrica
    Co-Authored-By: Agua-bit <60821161+Agua-bit@users.noreply.github.com>

--- src/compras/Fabrica.java
+++ src/compras/Fabrica.java
@@ -1,9 +1,56 @@
 package compras;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Fabrica {
+	private Bodega bodega;
+	private List<Producto> ListaDeProductosDiarios;
+	
+	
 
 	public Fabrica() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void eliminarProductoFabrica() {
+		
+	}
+	
+	public void agregarProductoNuevoFabrica() {
+		
+	}
+	public void agregarIngredienteNuevoFabrica() {
+		
+	}
+	public void establecerTiempoDemora() {
+		
+	}
+	public void pausarProduccion() {
+		
+	}
+	public String generarCodigoTanda() {
+		
+	}
+//	Getters y Setters
+
+	public Bodega getBodega() {
+		return bodega;
 	}
 
+	public void setBodega(Bodega bodega) {
+		this.bodega = bodega;
+	}
+
+	public List<Producto> getListaDeProductosDiarios() {
+		return ListaDeProductosDiarios;
+	}
+
+	public void setListaDeProductosDiarios(List<Producto> listaDeProductosDiarios) {
+		ListaDeProductosDiarios = listaDeProductosDiarios;
+	}
+
+
+	
 }
