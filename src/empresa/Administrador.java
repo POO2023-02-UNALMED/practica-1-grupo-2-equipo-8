c38788f20539ecf89a2587dd2ea5bdf17b0f5838
Agua-bit <minoltaguarin01@gmail.com>
2023-10-19 15:21:27 -0500

    Update Administrador.java

--- src/empresa/Administrador.java
+++ src/empresa/Administrador.java
@@ -1,6 +1,7 @@
 package empresa;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Administrador {
 
@@ -63,12 +64,43 @@ public class Administrador {
 
     //iniciarSesion(): Metodo que se ejecuta cada vez que el Administrador inicie sesiòn en cada interacciòn que tenga con el software
     private static String iniciarSesion(){
-        return "Iniciar Sesion";
+        
+
+        public static void iniciarSesion(String[] args){}
+        //Se crea un objeto Scanner para leer la respuesta del usuario al mensaje a continuacion
+        Scanner numero = new Scanner(System.in);
+
+        //Mensaje inicial del Software, dando la bienvenida al Administrador y dandole las opciones disponibles a elegir mediante numeros (del 1 al 5)
+        System.out.println("Bienvenido a Alimentos Delihorno. ¿Que desearìa hacer el dìa de hoy?:" + "\n" + "1. Comprar Materia Prima" + "\n"
+        + "2. Venta Por Encargo" + "\n" + "3. Cambiar Lista de Produccion Diaria" + "\n" + "4. Agregar Producto" + "\n" + "5. Eliminar Producto");
+
+        //Se lee la respuesta del usuario al mensaje anterior
+        int opcionElegida = numero.nextInt(); 
+
+        //De acuerdo al valor Integer de opcionElegida, se elige ejecutar uno de los metodos a continuacion
+        if (opcionElegida == 1){
+
+        } else if(opcionElegida == 2){
+
+            
+
+        } else if(opcionElegida == 3){
+
+        } else if (opcionElegida == 4){
+
+        } else if (opcionElegida == 5){
+            
+        };
+
+        //Retorno a eliminarse cuando se corrija el codigo
+        return null;
     }
 
     //acabarSesion(): Metodo que se ejecuta cada vez que termine la interaccion del Administrador con el software
     private String acabarSesion(){
-        return "Acabar Sesiòn";
+
+        //Mensaje de despedida
+        return "Muchas gracias por usar el software de DeliHorno. Que tenga un feliz dia.";
     }
     
 }
